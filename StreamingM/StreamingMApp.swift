770b5dbaa85cbab7abf61b5bf94cba3202562f40
CarmineFranzese <carmine.franzese.97@icloud.com>
2024-12-09 12:42:35 +0100

    Initial Commit

---
+++ StreamingM/StreamingMApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamingMApp.swift
+//  StreamingM
+//
+//  Created by Carmine Franzese on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct StreamingMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
